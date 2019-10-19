2d23efb118dcf320d8b49f72cccf04a85c02614b
Jerry Egbunu <jerryegbunu@Jerrys-MacBook-Pro.local>
2019-10-19 10:25:07 -0400

    today

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Learning functions 
+
+function Jaylen(JaylenObject) {
+	return 'Age: ' + JaylenObject["age"] + '; Skin color: ' + JaylenObject["color"] + '; Weight ' + JaylenObject["weight"];
+}
+
+function area(a, b) {
+	return a*b;
+}
